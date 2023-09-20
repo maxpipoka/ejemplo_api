6dd4866ff623b045e1a6c2448fa0a82c2dacbed7
maxpipoka <martincardozo@gmail.com>
2023-09-20 16:02:36 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,97 @@
+import { createRequire } from 'node:module'
+
+const require = createRequire(import.meta.url)
+const datos = require('./datos.json')
+
+import express from 'express'
+const html = '<h1>Bienvenido a la API</h1><p>Los comandos disponibles son:</p><ul><li>GET: /productos/</li><li>GET: /productos/id</li>    <li>POST: /productos/</li>    <li>DELETE: /productos/id</li>    <li>PUT: /productos/id</li>    <li>PATCH: /productos/id</li>    <li>GET: /usuarios/</li>    <li>GET: /usuarios/id</li>    <li>POST: /usuarios/</li>    <li>DELETE: /usuarios/id</li>    <li>PUT: /usuarios/id</li>    <li>PATCH: /usuarios/id</li></ul>'
+
+const app = express()
+
+const exposedPort = 1234
+
+app.get('/', (req, res) => {
+    res.status(200).send(html)
+})
+
+app.get('/productos/', (req, res) =>{
+    try {
+        let allProducts = datos.productos
+
+        res.status(200).json(allProducts)
+    } catch (error) {
+        res.status(204).send('No hay productos que mostrar.')
+    }
+})
+
+app.get('/productos/:id', (req, res) => {
+    try {
+        let productoId = parseInt(req.params.id)
+        let productoEncontrado = datos.productos.find((producto) => producto.id === productoId)
+
+        res.status(200).json(productoEncontrado)
+    } catch (error) {
+        res.status(204).send('No hay productos que mostrar.')
+    }
+})
+
+app.post('/productos/', (req, res) => {
+    let bodyTemp = ''
+
+    req.on('data', (chunk) => {
+        bodyTemp += chunk.toString()
+    })
+
+    req.on('end', () => {
+        const data = JSON.parse(bodyTemp)
+        req.body = data
+        datos.productos.push(req.body)
+    })
+
+    res.status(201).send('Producto agregado')
+})
+
+app.patch('/productos/:id', (req, res) => {
+    let idProductoAEditar = parseInt(req.params.id)
+    let productoEncontrado = datos.productos.find((producto) => producto.id === idProductoAEditar)
+
+    if (!productoEncontrado) {
+        res.status(204).send('Producto no encontrado')
+    }
+
+    let bodyTemp = ''
+
+    req.on('data', (chunk) => {
+        bodyTemp += chunk.toString()
+    })
+
+    req.on('end', () => {
+        const data = JSON.parse(bodyTemp)
+        
+        if(data.nombre){
+            productoEncontrado.nombre = data.nombre
+        }
+        
+        if (data.tipo){
+            productoEncontrado.tipo = data.tipo
+        }
+
+        if (data.precio){
+            productoEncontrado.precio = data.precio
+        }
+        datos.productos.push(productoEncontrado)
+    })
+
+})
+
+app.use((req, res) => {
+    res.status(404).send('<h1>404</h1>')
+})
+
+app.listen( exposedPort, () => {
+    console.log('Servidor escuchando en http://localhost:' + exposedPort)
+})
+
+
+
+
